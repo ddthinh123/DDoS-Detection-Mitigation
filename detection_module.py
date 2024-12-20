ad80a41791710884bd7153efc743eb0c938cbdfd
ddthinh123 <102498381+ddthinh123@users.noreply.github.com>
2024-12-20 16:25:31 +0700

    Update detection_module.py

--- detection_module.py
+++ detection_module.py
@@ -1,9 +1,1 @@
-class DDoSDetector:
-    def __init__(self, model):
-        self.model = model
-    
-    def detect(self, features):
-        # Chuyển đổi đặc trưng thành định dạng mà mô hình có thể nhận
-        prediction = self.model.predict(features)
-        return prediction == 1  # Nếu là tấn công DDoS, trả về True
 
